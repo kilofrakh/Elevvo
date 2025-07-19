9a1aafb8435773a30df5342f083617a3803de86a
abdelkareem <kikoabdelkareem@gmail.com>
2025-07-19 08:29:28 +0300

    .

---
+++ task_1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data =pd.read_csv('data.csv')
+
+data = data.dropna()  
+
+print(data.info())
+print(data.describe())
+
+z1 = data['Hours_Studied']
+z2 = data['Exam_Score']
+
+plt.scatter(z1, z2)
+plt.xlabel('Hours Studied')
+plt.ylabel('Exam Score')
